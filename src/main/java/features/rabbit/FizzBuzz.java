b702a3bddb302c538dfccd42bfa3ab97635fd5e3
emanon001 <emanon001@gmail.com>
2012-08-18 16:00:14 +0900

    [FizzBuzz] 0以下の数を与えた場合のコードをコメントアウト

--- src/main/java/features/rabbit/FizzBuzz.java
+++ src/main/java/features/rabbit/FizzBuzz.java
@@ -4,7 +4,7 @@ public class FizzBuzz implements features.FizzBuzz {
 
 	@Override
 	public String fizzBuzz(int num) {
-		if(num <= 0) throw new RuntimeException();
+		/* if(num <= 0) throw new RuntimeException(); */
 		if(num % 3 == 0 && num % 5 == 0) return "FizzBuzz";
 		if(num % 5 == 0) return "Buzz";
 		if(num % 3 == 0) return "Fizz";
